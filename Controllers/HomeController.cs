9953da0baacebd5d2c89304214fd8518ef1fb8c5
Ryan Henrique de Aguiar dos Santos <gtsupera12@gmail.com>
2022-08-16 17:57:00 -0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ExemploAspNetMvc.Models;
+
+namespace ExemploAspNetMvc.Controllers;
+
+//UserDto
+//UserCreateDto
+//UserViewModel
+//UserRequest
+
+public class UserRequest
+{
+   public string Nome  { get; set; }
+   public string Email { get; set; }
+}
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult PrimeiraAction()
+    {
+        return View();
+    }
+
+    public string TesteFormData([FromForm] UserRequest userRequest, [FromHeader] string valor )
+    {
+        return $"Nome: {userRequest.Nome} , Email: {userRequest.Email}, valor: {valor}";
+    }
+     public IActionResult Formulario()
+     {
+        return View();
+     }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
